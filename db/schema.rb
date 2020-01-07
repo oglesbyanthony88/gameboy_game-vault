c1dcf88a3b0d0364bf106f6854d21728decad70b
Anthony Oglesby <ogle1ant@gmail.com>
2020-01-07 16:31:50 -0500

    Added seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200107184025) do
+ActiveRecord::Schema.define(version: 20200107210754) do
 
   create_table "games", force: :cascade do |t|
-    t.text    "gamename"
-    t.text    "developer"
-    t.text    "publisher"
-    t.boolean "authentic"
-    t.text    "releasedate"
-    t.text    "condition"
+    t.text     "gamename"
+    t.text     "console_format"
+    t.text     "developer"
+    t.text     "publisher"
+    t.boolean  "authentic"
+    t.text     "releasedate"
+    t.text     "condition"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.text     "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "wishlist", force: :cascade do |t|
+    t.text     "gamename"
+    t.text     "console_format"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
