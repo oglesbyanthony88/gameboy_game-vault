60d3e60fc150004477a1a6644926afd137da1185
Anthony Oglesby <ogle1ant@gmail.com>
2020-01-07 17:26:04 -0500

    fix seed mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :games
+	has_many :wishlist
 	has_secure_password
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@ samus = User.create(username: "SamusRox", password: "Hello2")
 
 # Create 2 games each
 
-Games.create(gamename: "Mercenary Force", console_format: "DMG-001",
+kirby.Games.create(gamename: "Mercenary Force", console_format: "DMG-001",
  developer: "KAZe Co., Ltd.", publisher: "Meldac of Japan", authentic: true, 
- releasedate: "October 1990", condition: "Great", user_id: kirby.id)
+ releasedate: "October 1990", condition: "Great")
 
-Games.create(gamename: "Pocket Bomberman", console_format: "GBC",
+kirby.Games.create(gamename: "Pocket Bomberman", console_format: "GBC",
  developer: "Hudson Soft", publisher: "Nintendo", authentic: true, 
- releasedate: "November, 1998", condition: "Great", user_id: kirby.id)
+ releasedate: "November, 1998", condition: "Great")
 
-Games.create(gamename: "Metroid Fusion", console_format: "GBA",
+samus.Games.create(gamename: "Metroid Fusion", console_format: "GBA",
  developer: "Nintendo R&D1", publisher: "Nintendo", authentic: false, 
- releasedate: "November, 2002", condition: "Perfect", user_id: samus.id)
+ releasedate: "November, 2002", condition: "Perfect")
 
-Games.create(gamename: "Pokemon Blue", console_format: "GBC",
+samus.Games.create(gamename: "Pokemon Blue", console_format: "GBC",
  developer: "Game Freak", publisher: "Nintendo", authentic: true, 
- releasedate: "September, 1998", condition: "Good", user_id: samus.id)
+ releasedate: "September, 1998", condition: "Good")
 
 # Create wishlist for each
 
-Wishlist.create(gamename: "Pokemon Gold", console_format: "GBC", user_id: kirby.id)
-Wishlist.create(gamename: "The Legend of Zelda: Link's Awakening", console_format: "DMG-001", user_id: samus.id)
+kirby.Wishlist.create(gamename: "Pokemon Gold", console_format: "GBC", user_id: kirby.id)
+samus.Wishlist.create(gamename: "The Legend of Zelda: Link's Awakening", console_format: "DMG-001")
