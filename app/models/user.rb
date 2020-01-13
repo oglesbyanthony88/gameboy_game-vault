53c05b8cfa7fa03ef348cf06c0e92c0a0d684d1e
Anthony Oglesby <ogle1ant@gmail.com>
2020-01-13 11:03:26 -0500

    username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	
 	has_many :games
 	has_many :wishlist
+	validates :username, :password, presence: true
+    validates_uniqueness_of :username
 	has_secure_password
 
 end
