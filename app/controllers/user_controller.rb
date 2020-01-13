bbd98740fa370b6d1484ee1b2381d486d8fe281a
Anthony Oglesby <ogle1ant@gmail.com>
2020-01-13 11:58:37 -0500

    Added new flash error message

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect '/login'
     else
-      flash.now[:error] = "Invalid Username. Try again"
+      flash[:error] = "Invalid Username. Try again"
       redirect '/signup'
     end
   end
